ce6ca454cf90c343d58bb9bd2d99c548658901bb
kevoluke <kevoluke@gmail.com>
2023-02-10 14:38:52 -0800

    Task 8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,3 +41,29 @@ class FileStorage:
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+
+        classes = {"BaseModel": BaseModel,
+                "User": User}
+        return classes
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+         attributes = {
+                 "BaseModel":
+                 {"id": str,
+                     "created_at": datetime.datetime,
+                     "updated_at": datetime.datetime},
+
+                 "User":
+                {"email": str,
+                     "password": str,
+                     "first_name": str,
+                     "last_name": str}
+                }
+         return attributes
+
